9c0944d560e72132de5ae7dfbb8866dff05ad462
JimvandeVen <jimvandeven_1@hotmail.com>
2019-02-04 15:25:28 +0100

    changed the request to a fetch and added the card images to the render

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,21 +1,59 @@
-'use strict'
+"use strict";
 
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", renderCards, false);
 
-let request = new XMLHttpRequest()
+function renderCards() {
+  const app = document.querySelector("#root");
+  console.log("render");
 
-request.open("GET", "https://api.magicthegathering.io/v1/cards?page=311>", true)
+  let url = "https://api.magicthegathering.io/v1/cards";
 
-request.onload = function(){
-  let data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status< 400) {
-    data.cards.forEach(card=>{
-      console.log(card.name)
+  fetch(url)
+    .then(data => {
+      return data.json();
     })
-  } else {
-    console.log("error")
-  }
+    .then(res => {
+      res.cards.forEach(card => {
+        if (card.imageUrl !== undefined) {
+          const cardContainer = document.createElement("div");
+          cardContainer.setAttribute("class", "cardContainer");
+          app.appendChild(cardContainer);
 
-  // console.log("data", data)
+          const cardImg = document.createElement("img");
+          cardImg.src = card.imageUrl;
+          cardContainer.appendChild(cardImg);
+        }
+      });
+    });
 }
 
-request.send()
+// 0:
+// artist: "Steve Prescott"
+// cmc: 2
+// colorIdentity: ["U"]
+// colors: ["Blue"]
+// foreignNames: (8) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// id: "fab936d6-1de4-5182-916a-1ead4f764d8f"
+// imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=122325&type=card"
+// layout: "normal"
+// legalities: (7) [{…}, {…}, {…}, {…}, {…}, {…}, {…}]
+// manaCost: "{1}{U}"
+// multiverseid: 122325
+// name: "Riptide Pilferer"
+// number: "60"
+// originalText: "Whenever Riptide Pilferer deals combat damage to a player, that player discards a card.↵Morph {U} (You may play this face down as a 2/2 creature for {3}. Turn it face up any time for its morph cost.)"
+// originalType: "Creature - Merfolk Rogue"
+// power: "1"
+// printings: ["PLC"]
+// rarity: "Uncommon"
+// rulings: [{…}]
+// set: "PLC"
+// setName: "Planar Chaos"
+// subtypes: (2) ["Merfolk", "Rogue"]
+// supertypes: []
+// text: "Whenever Riptide Pilferer deals combat damage to a player, that player discards a card.↵Morph {U} (You may cast this card face down as a 2/2 creature for {3}. Turn it face up any time for its morph cost.)"
+// toughness: "1"
+// type: "Creature — Merfolk Rogue"
+// types: ["Creature"]
+// __proto__: Object
