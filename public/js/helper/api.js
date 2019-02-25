0635d9d8f6be80344e83a7d0a9982d09ed7db444
JimvandeVen <jimvandeven_1@hotmail.com>
2019-02-25 16:19:21 +0100

    fixed the config

--- public/js/helper/api.js
+++ public/js/helper/api.js
@@ -3,16 +3,17 @@
 import { emptyDOM, renderCards } from "../cards.js"
 import { addLoading } from "../../loading.js"
 
-const config = {
-  endpoint:"https://api.magicthegathering.io/v1/cards",
-  cmc: document.querySelector("#CMC").value,
-  rarity: document.querySelector("#rarity").value,
-  type: document.querySelector("#type").value,
-  colors: document.querySelector("#colors").value
-}
-
 async function getData() {
   addLoading()
+
+  const config = {
+    endpoint:"https://api.magicthegathering.io/v1/cards",
+    cmc: document.querySelector("#CMC").value,
+    rarity: document.querySelector("#rarity").value,
+    type: document.querySelector("#type").value,
+    colors: document.querySelector("#colors").value
+  }
+  
   let url = `${config.endpoint}?types=${config.type}&rarity=${config.rarity}&cmc=${config.cmc}&colors=${config.colors}`;
 
   let result = await fetch(url)
